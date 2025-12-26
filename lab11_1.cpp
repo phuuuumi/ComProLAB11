ad272da64e6e8e9284adc66ebf6e4cac14a1ddbd
D <phuuuumi@gmail.com>
2025-12-26 14:50:49 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main(){
+    string grades[] = { "A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0; i<3; i++){
+        cin.get();
+    }
+
+    srand(time(0));
+
+    cout << "You will get " << grades[rand() % 9] << " in this 261102.";
+
+}
+
